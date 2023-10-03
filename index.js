421daa2aee33a1a94d8d0750290ad5b26b623da2
TRISTAN ZVUNKA <tristan.zvunka@gmail.com>
2023-10-03 10:43:53 +0200

    added files

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.red("Hello Doriane !!!"));
+console.log(chalk.yellow("Hello Baptiste !!!"));
+console.log(chalk.gray("Hello Raphael !!!"));
+console.log(chalk.green("Hello Jonathan !!!"));
